ca1ac37a77f6529f901a57503e1cb1a2529ca19d
vrunoa <vruno.alassia@gmail.com>
2016-02-01 11:27:18 -0800

    changing scope values and running tests passed

--- tests/test.js
+++ tests/test.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 })
 app.listen(5000)
 
-let should = chai.should()
+let should = chai.should(), expect = chai.expect
 let controllerName = "MarController"
 
 describe("Test ", () => {
@@ -20,11 +20,35 @@ describe("Test ", () => {
   let driver = new webdriver.Builder()
     .withCapabilities(webdriver.Capabilities.chrome())
     .build()
+  let mar = new Mar(driver, controllerName)
+
 
   after( () => {
     return driver.quit()
   })
 
+  it("Test getScopeSelector", (done) => {
+    let mar = new Mar(driver, controllerName)
+    let selector = mar.getScopeSelector()
+    selector.should.not.equal(null)
+    selector.should.not.equal(undefined)
+    expect(selector).to.be.a("string")
+    expect(selector).to.match(/\[ng\-controller\=\"[\w\d\-]+\"\]/)
+    done()
+  })
+
+  it("Test getExecpScript", (done) => {
+    let mar = new Mar(driver, controllerName)
+    let selector = mar.getScopeSelector()
+    let obj = {q:"query"}
+    let script = mar.getScript(selector, obj)
+    script.should.not.equal(null)
+    script.should.not.equal(undefined)
+    expect(script).to.be.a("string")
+    done()
+    // expect(script).to.match(new RegExp(`^var scope \= angular\.element(document\.querySelectorAll(\'${selector}'))\.scope\;.+`))
+  })
+  
   it("Test using Mar", (done) => {
     driver
     .get("http://localhost:5000")
@@ -40,10 +64,26 @@ describe("Test ", () => {
     .then( (is) => {
       is.should.equal(false)
     })
-    // research how to get window.document from driver
-    let mar = new Mar(driver, controllerName)
+
+    // make mar to extend webdriver prototype
     mar.apply({mustShow:true})
+    .then( () => {
+      driver
+        .findElement(By.id("sopa"))
+        .isDisplayed()
+        .then( (is) => {
+        is.should.equal(false)
+      })
+      driver
+        .findElement(By.id("show"))
+        .isDisplayed()
+        .then( (is) => {
+        is.should.equal(true)
+        done()
+      })
+    })
     
   })
+
 
 })
